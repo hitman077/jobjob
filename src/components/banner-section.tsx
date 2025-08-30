476f9082dc0cf2486229ac2122df96fae795f5fb
iam_em150 <ntw3840@gmail.com>
2025-08-30 15:23:11 +0700

    add header

---
+++ src/components/banner-section.tsx
@@ -0,0 +1,71 @@
+"use client"
+import React from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import {
+	Autoplay,
+	EffectCoverflow,
+	Navigation,
+	Pagination,
+} from "swiper/modules"
+import "swiper/css"
+import "swiper/css/autoplay"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/effect-coverflow"
+
+export default function BannerSection() {
+	return (
+		<section className="py-10">
+			<div className="mx-auto max-w-5xl px-6">
+				<div className="x-auto relative max-w-5xl">
+					<Swiper
+						slidesPerView={1}
+						pagination={{ clickable: true }}
+						loop
+						autoplay={{ delay: 5000 }}
+                        navigation={false}
+                        
+						// effect="coverflow"
+						modules={[
+							Autoplay,
+							EffectCoverflow,
+							Navigation,
+							Pagination,
+						]}
+					>
+						<SwiperSlide className="px-2">
+							<div className="bg-amber-500 rounded-(--radius) h-70 max-w-5xl border p-9">
+								<div className="mx-auto h-fit w-full">
+									{/* <NetlifyLogo /> */}
+								</div>
+								<p className="mt-6 text-center text-lg font-medium">
+									30% Increase in revenue
+								</p>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="px-2">
+							<div className="bg-background rounded-(--radius) h-70 max-w-5xl border p-9">
+								<div className="mx-auto h-fit w-full">
+									{/* <AstroLogo /> */}
+								</div>
+								<p className="mt-6 text-center text-lg font-medium">
+									45% Increase in revenue
+								</p>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide className="px-2">
+							<div className="bg-background rounded-(--radius) h-70 max-w-5xl border p-9">
+								<div className="mx-auto h-fit w-full">
+									{/* <WorkOsLogo /> */}
+								</div>
+								<p className="mt-6 text-center text-lg font-medium">
+									60% Increase in revenue
+								</p>
+							</div>
+						</SwiperSlide>
+					</Swiper>
+				</div>
+			</div>
+		</section>
+	)
+}
---
+++ src/components/job/job-section.tsx
@@ -0,0 +1,67 @@
+import { Check } from 'lucide-react'
+import React from 'react'
+import { Button } from '../ui/button'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '../ui/card'
+import Link from 'next/link'
+
+
+
+export default function JobSection() {
+  return (
+		<section className="py-10">
+			<div className="mx-auto max-w-5xl px-6">
+				<div className="mx-auto max-w-5xl space-y-6">
+					<h1 className="text-xl font-semibold lg:text-2xl">
+						TOP COMPANIES
+					</h1>
+				</div>
+
+              <div className="mt-4 grid gap-3 md:mt-4 md:grid-cols-5">
+                  {[1,2,3,4,5].map((item, index) => (
+					<Card className="flex flex-col" key={index}>
+						<CardHeader>
+							<CardTitle className="font-medium">Free</CardTitle>
+							<span className="my-3 block text-2xl font-semibold">
+								$0 / mo
+							</span>
+							<CardDescription className="text-sm">
+								Per editor
+							</CardDescription>
+						</CardHeader>
+
+						<CardContent className="space-y-4">
+							<hr className="border-dashed" />
+
+							<ul className="list-outside space-y-3 text-sm">
+								{[
+									"Basic Analytics Dashboard",
+									"5GB Cloud Storage",
+									"Email and Chat Support",
+								].map((item, index) => (
+									<li
+										key={index}
+										className="flex items-center gap-2"
+									>
+										<Check className="size-3" />
+										{item}
+									</li>
+								))}
+							</ul>
+						</CardContent>
+
+						<CardFooter className="mt-auto">
+							<Button
+								asChild
+								variant="outline"
+								className="w-full"
+							>
+								<Link href="">Get Started</Link>
+							</Button>
+						</CardFooter>
+                      </Card>
+                      ))}
+				</div>
+			</div>
+		</section>
+  )
+}
