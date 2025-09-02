f67644724b73f82c95f7490a7242324eb63e5b0b
iam_em150 <ntw3840@gmail.com>
2025-09-02 15:35:16 +0700

    update banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import BannerSection from "@/components/banner-section";
 import FooterSection from "@/components/footer";
+import HeroSection from "@/components/hero-section";
 import JobSection from "@/components/job/job-section";
 import JobSection2 from "@/components/job/job-section2";
 import JobSection3 from "@/components/job/job-section3";
@@ -8,6 +9,7 @@ import JobSection4 from "@/components/job/job-section4";
 export default function Home() {
   return (
     <>
+      {/* <HeroSection /> */}
       <BannerSection />
 			<JobSection />
 			<JobSection2 />
--- src/components/banner-section.tsx
+++ src/components/banner-section.tsx
@@ -14,6 +14,24 @@ import "swiper/css/pagination"
 import "swiper/css/effect-coverflow"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 
+const banner = [
+	{
+		id: 1,
+		imgUrl: "/images/1.webp",
+	},
+	{
+		id: 2,
+		imgUrl: "/images/2.webp",
+	},
+	{
+		id: 3,
+		imgUrl: "/images/3.webp",
+	},
+	// {
+	// 	id: 4,
+	// 	imgUrl: "/images/news/n1.jpg",
+	// },
+]
 export default function BannerSection() {
 	return (
 		<section className="py-10">
@@ -39,36 +57,26 @@ export default function BannerSection() {
 							Pagination,
 						]}
 					>
-						<SwiperSlide className="">
+						{banner.map((item, index) => (
+							<SwiperSlide>
+								{/* <div className="relative rounded-md overflow-hidden"> */}
+								<img
+									className="w-full size-full object-fill"
+									src={item.imgUrl}
+									key={item.id}
+								/>
+								{/* </div> */}
+							</SwiperSlide>
+						))}
+						{/* <SwiperSlide className="">
 							<div className="bg-amber-500 rounded-(--radius) h-80 max-w-6xl border p-9">
 								<div className="mx-auto h-fit w-full">
-									{/* <NetlifyLogo /> */}
 								</div>
 								<p className="mt-6 text-center text-lg font-medium">
 									30% Increase in revenue
 								</p>
 							</div>
-						</SwiperSlide>
-						<SwiperSlide className="">
-							<div className="bg-pink-500 rounded-(--radius) h-80 max-w-6xl border p-9">
-								<div className="mx-auto h-fit w-full">
-									{/* <AstroLogo /> */}
-								</div>
-								<p className="mt-6 text-center text-lg font-medium">
-									45% Increase in revenue
-								</p>
-							</div>
-						</SwiperSlide>
-						<SwiperSlide className="">
-							<div className="bg-emerald-500 rounded-(--radius) h-80 max-w-6xl border p-9">
-								<div className="mx-auto h-fit w-full">
-									{/* <WorkOsLogo /> */}
-								</div>
-								<p className="mt-6 text-center text-lg font-medium">
-									60% Increase in revenue
-								</p>
-							</div>
-						</SwiperSlide>
+						</SwiperSlide> */}
 						<div className="swiper-button-prev absolute top-1/2 -left-2 z-10 -translate-y-1/2 w-10 h-15 flex items-center justify-center rounded-r-md bg-black/40 text-white cursor-pointer opacity-0 group-hover:opacity-100 duration-300 hover:bg-black/70">
 							<ChevronLeft className="size-7" />
 						</div>
