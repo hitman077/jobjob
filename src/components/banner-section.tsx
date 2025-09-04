97a5b5cf938690edb87fc8656dd911f177dfb151
iam_em150 <ntw3840@gmail.com>
2025-09-04 09:48:54 +0700

    Update banner-section.tsx

--- src/components/banner-section.tsx
+++ src/components/banner-section.tsx
@@ -3,7 +3,7 @@
 import { Swiper, SwiperSlide } from "swiper/react"
 import {
 	Autoplay,
-	EffectCoverflow,
+	EffectFade,
 	Navigation,
 	Pagination,
 } from "swiper/modules"
@@ -11,8 +11,9 @@ import "swiper/css"
 import "swiper/css/autoplay"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
-import "swiper/css/effect-coverflow"
+import "swiper/css/effect-fade"
 import { ChevronLeft, ChevronRight } from "lucide-react"
+import { Skeleton } from "./ui/skeleton"
 
 const banner = [
 	{
@@ -23,10 +24,10 @@ const banner = [
 		id: 2,
 		imgUrl: "https://placehold.co/990x334/259C07/FFFFFF.png",
 	},
-	{
-		id: 3,
-		imgUrl: "https://placehold.co/990x334/0933C8/FFFFFF.png",
-	},
+	// {
+	// 	id: 3,
+	// 	imgUrl: "https://placehold.co/990x334/0933C8/FFFFFF.png",
+	// },
 	// {
 	// 	id: 4,
 	// 	imgUrl: "/images/news/n1.jpg",
@@ -34,58 +35,51 @@ const banner = [
 ]
 export default function BannerSection() {
 	return (
-		<section className="pt-16 md:pt-10">
-			<div className="mx-auto max-w-6xl px-6">
-				<div className="x-auto relative max-w-6xl">
-					<Swiper
-						className="swiper relative rounded-lg overflow-hidden shadow-lg group"
-						spaceBetween={0}
-						slidesPerView={1}
-						pagination={{ dynamicBullets: true }}
-						// navigation={true}
-						navigation={{
-							prevEl: ".swiper-button-prev",
-							nextEl: ".swiper-button-next",
-						}}
-						loop
-						autoplay={{ delay: 5000 }}
-						// effect="coverflow"
-						modules={[
-							Autoplay,
-							EffectCoverflow,
-							Navigation,
-							Pagination,
-						]}
-					>
-						{banner.map((item, index) => (
-							<SwiperSlide>
-								{/* <div className="relative rounded-md overflow-hidden"> */}
-								<img
-									className="w-full size-full object-fill"
-									src={item.imgUrl}
-									key={item.id}
-								/>
-								{/* </div> */}
-							</SwiperSlide>
-						))}
-						{/* <SwiperSlide className="">
-							<div className="bg-amber-500 rounded-(--radius) h-80 max-w-6xl border p-9">
-								<div className="mx-auto h-fit w-full">
-								</div>
-								<p className="mt-6 text-center text-lg font-medium">
-									30% Increase in revenue
-								</p>
-							</div>
-						</SwiperSlide> */}
+		<div className="relative overflow-hidden">
+			{/* <section className="relative">
+				<div className="relative py-24 lg:py-28">
+					<div className="mx-auto max-w-7xl px-6 md:px-12"> */}
+			<Swiper
+				className="group"
+				spaceBetween={0}
+				slidesPerView={1}
+				pagination={{ dynamicBullets: true }}
+				// navigation={true}
+				navigation={{
+					prevEl: ".swiper-button-prev",
+					nextEl: ".swiper-button-next",
+				}}
+				loop
+				autoplay={{ delay: 5000 }}
+				effect="fade"
+				modules={[Autoplay, EffectFade, Navigation, Pagination]}
+			>
+				{banner.map((item, index) => (
+					<SwiperSlide>
+						{/* <img
+							className="w-full size-full object-fill"
+							src={item.imgUrl}
+							key={item.id}
+						/> */}
+						<Skeleton className="flex h-[455px] w-full text-8xl items-center justify-center rounded-none">
+							900 x 455
+						</Skeleton>
+					</SwiperSlide>
+				))}
+				{banner.length > 1 && (
+					<>
 						<div className="swiper-button-prev absolute top-1/2 -left-2 z-10 -translate-y-1/2 w-10 h-15 flex items-center justify-center rounded-r-md bg-black/40 text-white cursor-pointer opacity-0 group-hover:opacity-100 duration-300 hover:bg-black/70">
 							<ChevronLeft className="size-7" />
 						</div>
 						<div className="swiper-button-next absolute top-1/2 -right-2 z-10 -translate-y-1/2 w-10 h-15 flex items-center justify-center rounded-l-md bg-black/40 text-white cursor-pointer opacity-0 group-hover:opacity-100 duration-300 hover:bg-black/70">
 							<ChevronRight className="size-7" />
 						</div>
-					</Swiper>
+					</>
+				)}
+			</Swiper>
+			{/* </div>
 				</div>
-			</div>
-		</section>
+			</section> */}
+		</div>
 	)
 }
