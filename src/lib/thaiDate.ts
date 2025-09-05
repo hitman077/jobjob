171a7066c71e4bad93a1386040aa8b5c2d987b7a
iam_em150 <ntw3840@gmail.com>
2025-09-05 16:58:24 +0700

    Create thaiDate.ts

---
+++ src/lib/thaiDate.ts
@@ -0,0 +1,20 @@
+import dayjs from "dayjs"
+import buddhistEra from "dayjs/plugin/buddhistEra"
+import th from "dayjs/locale/th"
+
+// ตั้งค่า locale และ plugin
+dayjs.extend(buddhistEra)
+dayjs.locale(th)
+
+// สร้างฟังก์ชันสำหรับแปลงวันที่เป็นภาษาไทย
+export const formatThaiDate = (date:any) => {
+	if (!date) return ""
+	return dayjs(date).format("D MMMM BBBB")
+	// return dayjs(date).format("วันddddที่ DD MMMM BBBB เวลา H:mm น.")
+}
+
+// สร้างฟังก์ชันสำหรับรับปีพุทธศักราช
+export const getThaiBuddhistYear = (date) => {
+	if (!date) return null
+	return dayjs(date).buddhistYear()
+}
