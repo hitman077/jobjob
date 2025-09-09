25b1dffbed59c7f58063b3ff3dd1276740086730
iam_em150 <ntw3840@gmail.com>
2025-09-09 10:43:55 +0700

    Update thaiDate.ts

--- src/lib/thaiDate.ts
+++ src/lib/thaiDate.ts
@@ -9,7 +9,7 @@ dayjs.locale(th)
 // สร้างฟังก์ชันสำหรับแปลงวันที่เป็นภาษาไทย
 export const formatThaiDate = (date:any) => {
 	if (!date) return ""
-	return dayjs(date).format("D MMMM BBBB")
+	return dayjs(date).format("D MMM BBBB")
 	// return dayjs(date).format("วันddddที่ DD MMMM BBBB เวลา H:mm น.")
 }
 
