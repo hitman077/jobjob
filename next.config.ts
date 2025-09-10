0550d12cdcad6ae097b9691b8267ae306e397805
iam_em150 <ntw3840@gmail.com>
2025-09-10 15:51:51 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
 		remotePatterns: [
 			{ hostname: "i.imgur.com", protocol: "https" },
 			{ hostname: "picsum.photos", protocol: "https" },
+			{
+				hostname: "mercular.s3.ap-southeast-1.amazonaws.com",
+				protocol: "https",
+			},
 			// { hostname: "www.jobbkk.com", protocol: "https" },
 		],
 	},
