46b9728e3c895ba8fe2612ecea4e392957a30d60
iam_em150 <ntw3840@gmail.com>
2025-09-10 10:14:43 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       {/* <HeroSection /> */}
       <BannerSection />
 			<JobSection />
-			<JobSection2 />
-      {/* <JobSection3 /> */}
+			{/* <JobSection2 /> */}
+      <JobSection3 />
       {/* <JobSection4 /> */}
       <FooterSection/>
 		</>
